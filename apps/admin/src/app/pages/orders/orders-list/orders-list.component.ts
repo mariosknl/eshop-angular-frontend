fb037b6126a9d2da085b30d278f5d0059a13a5e0
Marios Kanellopoulos <mariosknl@outlook.com>
2023-02-24 10:12:05 +0200

    order status

--- apps/admin/src/app/pages/orders/orders-list/orders-list.component.ts
+++ apps/admin/src/app/pages/orders/orders-list/orders-list.component.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Order, OrdersService } from '@bluebits/orders';
 
+const ORDER_STATUS = {
+  0: {
+    label: 'Pending',
+    color: 'primary',
+  },
+  1: {
+    label: 'Processed',
+    color: 'warning',
+  },
+  2: {
+    label: 'Shipped',
+    color: 'warning',
+  },
+  3: {
+    label: 'Delivered',
+    color: 'Success',
+  },
+  4: {
+    label: 'Failed',
+    color: 'danger',
+  },
+};
+
 @Component({
   selector: 'admin-orders-list',
   templateUrl: './orders-list.component.html',
 })
 export class OrdersListComponent implements OnInit {
   orders: Order[] = [];
+  orderStatus = ORDER_STATUS;
 
   constructor(private ordersService: OrdersService) {}
 
